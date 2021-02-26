b434b219a165b06974fa8694db7d8d334f2f8871
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2021-02-25 22:25:24 -0600

    Add view to manage Customer List and creation

--- customer_admin/customers/views.py
+++ customer_admin/customers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-# Create your views here.
+from .models import Customer
+from .forms import CustomerForm
+
+
+class CustomerView(ListView):
+    model = Customer
+    paginate_by = 2
+    template_name = 'customers/index.html'
+
+
+class CreateCustomerView(CreateView):
+    template_name = 'customers/create_user_form.html'
+    form_class = CustomerForm
+    success_url = reverse_lazy('customer-view')
