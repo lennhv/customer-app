96d2213c6539b7a4db32eec15f9ceb0c062d6d92
Lenin Adin Hernández Vázquez <lennhv@gmail.com>
2021-02-25 22:19:29 -0600

    Model for customer

--- customer_admin/customers/models.py
+++ customer_admin/customers/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Customer(models.Model):
+    name = models.CharField(_('name'), max_length=100)
+    address = models.CharField(_('address'), max_length=200)
+    creation_at = models.DateTimeField(_('creation at'), auto_now_add=True)
+
+    def __str__(self):
+        return self.name
